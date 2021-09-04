9365b7b088aeccb3388d21bb7e2be3e426272b05
Osman Solomon <39956261+OzmanSolomon@users.noreply.github.com>
2021-09-04 09:58:34 +0400

    GeometryReader

---
+++ MyGeometryReader/MyGeometryReader/MyGeometryReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGeometryReaderApp.swift
+//  MyGeometryReader
+//
+//  Created by Osman Solomon on 04/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct MyGeometryReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
